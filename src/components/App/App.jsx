bc53c0156029e6f72abba6edf4e301a4d24e9498
Oleg <plumbumbox4@gmail.com>
2023-01-01 23:19:38 +0100

    refactor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,39 +13,57 @@ import {
 	ForgottenPassword,
 	SinglePainting,
 	News,
-	SingleNews
+	SingleNews,
 } from '../../pages/indexPage';
 
 import './app.scss';
 import '../../scss/style.scss';
 
 const routes = [
-	{ 
-		path: '', element: <Home />, nodeRef: createRef() 
+	{
+		path: '',
+		element: <Home />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/Authors', element: <Authors />, nodeRef: createRef() 
+	{
+		path: '/Authors',
+		element: <Authors />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/Authors/AuthorInfo/:id', element: <AboutAuthor />, nodeRef: createRef() 
+	{
+		path: '/Authors/AuthorInfo/:id',
+		element: <AboutAuthor />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/PersonalOffice', element: <UserAccount />, nodeRef: createRef() 
+	{
+		path: '/PersonalOffice',
+		element: <UserAccount />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/Author/SinglePainting/:id', element: <SinglePainting />, nodeRef: createRef() 
+	{
+		path: '/Author/SinglePainting/:id',
+		element: <SinglePainting />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/News', element: <News />, nodeRef: createRef() 
+	{
+		path: '/News',
+		element: <News />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/News/SingleNews/:id', element: <SingleNews />, nodeRef: createRef() 
+	{
+		path: '/News/SingleNews/:id',
+		element: <SingleNews />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '/ForgottenPassword', element: <ForgottenPassword />, nodeRef: createRef() 
+	{
+		path: '/ForgottenPassword',
+		element: <ForgottenPassword />,
+		nodeRef: createRef(),
 	},
-	{ 
-		path: '*', element: <Error404 />, nodeRef: createRef() 
+	{
+		path: '*',
+		element: <Error404 />,
+		nodeRef: createRef(),
 	},
 ];
 
@@ -56,7 +74,9 @@ const App = () => {
 				<Routes>
 					<Route path="/" element={<MainLayout />}>
 						{routes.map((route) => {
-							return <Route key={route.path} path={route.path} element={route.element} />
+							return (
+								<Route key={route.path} path={route.path} element={route.element} />
+							);
 						})}
 					</Route>
 				</Routes>
